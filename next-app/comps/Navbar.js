1af56faf237649b4c581923590b338e2c8595eb7
Asemota Ayevbosa <asemotamaki1@gmail.com>
2024-02-13 04:19:30 +0100

    updated

---
+++ next-app/comps/Navbar.js
@@ -0,0 +1,13 @@
+function Navbar() {
+      return (
+            <nav>
+                  <div className="logo">
+                        <h1>Dev List</h1>
+                  </div>
+                  <a>Home</a>
+                  <a>About</a>
+                  <a>Dev Listing</a>
+            </nav>
+      )
+}
+export default Navbar;
